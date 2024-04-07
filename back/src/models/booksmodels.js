5969f2942294082c48525cff0d9eea351ebc9b91
albertohdz021 <79125777+albertohdz021@users.noreply.github.com>
2024-04-08 05:44:06 +0800

    Create booksmodels.js

---
+++ back/src/models/booksmodels.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookSchema = new Schema({
+    titulo: String,
+    portada: String,
+    descripcion:String,
+    precio: Number,
+    autor: String,
+    fecha_salida:String,
+    cantidad:Number,
+    resena:Number
+});
+module.exports =mongoose.model("libros",bookSchema);
