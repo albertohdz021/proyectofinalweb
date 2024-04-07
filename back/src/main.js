c472120e7c9d026f70feb6a33868685feb372b9b
albertohdz021 <79125777+albertohdz021@users.noreply.github.com>
2024-04-08 05:38:46 +0800

    Create main.js

---
+++ back/src/main.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');   
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/biblioteca', {});
+const User=require('./Models/UserModel')
+app.use(cors());
+const auth=require('./middleware/auth')
+
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Registra errores para depuración
+    res.status(500).json({ error: 'Error interno del servidor' });
+  });
+
+app.use(express.json());
+app.post('/login', async(req, res) => {
+    try{
+        const { mail, password } = req.body;
+    const user= await User.findOne({mail});
+    if (!user) {
+        return res.status(401).json({ error: 'mail' });
+    }
+    if (user.password !== password) {
+        return res.status(401).json({ error: 'password' });
+    }
+    const token = jwt.sign({ id: user._id }, 'secretkey', { expiresIn: '1h' });
+    res.status(200).json({ token });
+    }catch(error){
+        res.send(error)
+        return res.status(401).json({ error: 'nothing' });
+    }
+    
+
+})
+//app.use(auth)
+app.use(require('./Routes/routes'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
